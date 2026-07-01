d6f525d2d14e492a3ac315360ed4c13df87034c7
vintage1500 <130380721+vintage1500@users.noreply.github.com>
2026-07-01 19:39:25 +0300

    Внес правки в код

--- task_manager2.js
+++ task_manager2.js
@@ -1,13 +1,11 @@
-// README
-// Единственное что меня смущает, насколько правильно объединить вывод завершенных и незавершенных задач в одну функцию или лучше было 
-// разделить на две отдельные функции с учетом одинакового наполнения функций
-// А также стоит ли таким же образом объединять очистку массивов
-let tasks = [];
-let completedTasks = [];
+let tasks = []; 
 
-// Конвертация даты в красивый формат
-function convertDate(date) {
-    return date.toLocaleString("ru-RU", { dateStyle: "full", timeStyle: "short" });
+// Перевод даты в красивый формат
+function formatDateTimeRu(date) {
+    return date.toLocaleString("ru-RU", {
+        dateStyle: "full",
+        timeStyle: "short"
+    });
 }
 
 // Создание объекта задачи
@@ -18,7 +16,7 @@ function createTask(title, description) {
         isCompleted: false,
         createdDate: new Date(),
         completedDate: null
-    } 
+    }
 }
 
 // Добавление объекта задачи в массив
@@ -27,99 +25,128 @@ function setTask(title, description = '') {
         console.log('Ошибка. Для создания задачи укажите название\n');
         return
     }
+
     const task = createTask(title.trim(), description.trim());
     tasks.push(task);
     console.log((`Задача №${tasks.length} добавлена \n`));
 }
 
-// Вывод списка задач/списка завершенных задач (по дефолту списка задач)
-function showTask(arr = tasks) {
-    console.log((arr === tasks) ? 'Список задач' : 'Список завершенных задач');
-    if (arr.length === 0) {
+// Вывод списка завершенных/незавершенных задач
+function showTask(showCompleted = false) {
+    const filteredTasks = tasks.filter(task => task.isCompleted === showCompleted);
+    
+    console.log(showCompleted ? 'Список завершенных задач:' : 'Список текущих задач:');
+
+    if (filteredTasks.length === 0) {
         console.log('Задач нет\n');
-        return    
+        return
     }
-    arr.forEach((task, i) => {
-        console.log(`Задача №${ i + 1}`);
-        console.log(`Название задачи: ${task.title}`);;
+
+    filteredTasks.forEach((task, i) => {
+        console.log(`Задача №${i + 1}`);
+        console.log(`Название задачи: ${task.title}`);
         console.log(`Описание задачи: ${task.description}`);
-        console.log((task.isCompleted === true) ? 'Задача завершена' : 'Задача не завершена');
-        console.log(`Задача создана в ${convertDate(task.createdDate)}`);
-        console.log((task.completedDate !== null) ? 'Задача завершена в ' + convertDate(task.completedDate) : 'Задача не завершена');
+        console.log(task.isCompleted ? 'Задача завершена' : 'Задача не завершена');
+        console.log(`Задача создана в ${formatDateTimeRu(task.createdDate)}`);
+
+        if (task.completedDate !== null) {
+            console.log(`Задача завершена в ${formatDateTimeRu(task.completedDate)}`);
+        } else {
+            console.log('Задача не завершена');
+        }
+
         console.log(); // Пустая строка для разделения
     })
-} 
+}
 
 // Завершение задачи
 function completeTask(index) {
     const task = tasks[index];
+
     if (!task) {
         console.log(`Задачи ${index + 1} не существует\n`);
-        return
+        return;
     }
+
+    if (task.isCompleted) {
+        console.log(`Задача №${index + 1} уже завершена\n`);
+        return;
+    }
+
     task.isCompleted = true;
-    task.completedDate = new Date();
-    completedTasks.push(task);
-    tasks.splice(index, 1);
-    console.log(`Задача №${index + 1} завершена`); 
-    console.log(`Количество завершенных задач: ${completedTasks.length}`)
+    task.completedDate = new Date(); 
+
+    const completedCount = tasks.filter(task => task.isCompleted).length;
+
+    console.log(`Задача №${index + 1} завершена`);
+    console.log(`Количество завершенных задач: ${completedCount}\n`);
 }
 
 // Удаление задачи по индексу
 function deleteTask(index) {
     const task = tasks[index];
+
     if (!task) {
         console.log(`Задачи ${index + 1} не существует`);
         return
     }
+
     if (task.isCompleted === false) {
         const isConfirmed = confirm(`Задача не выполнена, удалить?`);
-        if (isConfirmed) {
-            tasks.splice(index, 1);
-            console.log(`Задача №${index + 1} удалена \n`);
-        } else {
+
+        if (!isConfirmed) {
             console.log(`Удаление отменено. Задача №${index + 1} оставлена \n`);
+            return
         }
-    } else {
+
         tasks.splice(index, 1);
-        console.log(`Задача №${index + 1} удалена\n`);     
+        console.log(`Задача №${index + 1} удалена \n`);
+        return
     }
+
+    tasks.splice(index, 1);
+    console.log(`Задача №${index + 1} удалена\n`); 
 }
 
 // очистка задач
-function clearTasks() {
-    tasks = [];
-    console.log('Список задач очищен \n');
-}
+function clearTasks(clearCompleted = false) {
+    if (!clearCompleted) {
+        tasks = [];
+        console.log('Список всех задач очищен \n');
+        return;
+    }
 
-// очистка завершенных задач
-function clearCompletedTasks() {
-    completedTasks = [];
-    console.log('Список завершенных задач очищен\n');
+    tasks = tasks.filter(task => !task.isCompleted);
+    console.log('Список завершенных задач очищен \n');
 }
 
 // Блок тестирования
 // 1. Создание задач, в том числе в случае, когда поле title пустое
-setTask('title', 'description');
-setTask('title1', 'description1');
-setTask('title2', 'description2');
-setTask('', 'description2');
+setTask('task 1', 'description of task 1');
+setTask('task 2', 'description of task 2');
+setTask('task 3', 'description of task 3');
+setTask('task 4', 'description of task 4');
+setTask('', 'description of task with empty title');
+
 showTask();
 
 // Завершение задач, в том числе несуществующей
 completeTask(1);
 completeTask(4);
+
 showTask();
+showTask(true);
 
 // Удаление не работает в среде разработки Enterly, т.к. не поддерживается confirm
-// deleteTask(2);
-showTask();
+deleteTask(2); 
 
-// Очистка задач
+// Очистка завершенных задач
+
+clearTasks(true);
+showTask();
+showTask(true);
+
+// Очистка всех задач
+completeTask(1);
 clearTasks();
 showTask();
-
-// Очистка завершенны задач
-showTask(completedTasks);
-clearCompletedTasks();
-showTask(completedTasks);
